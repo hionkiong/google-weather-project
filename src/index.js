0505a3c116dcd8878190eba37f4627ef9b89bcd3
hionkiong <stellachoi007@gmail.com>
2020-09-23 22:20:30 +0100

    Updated to show live temperature and time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "d20ee31d1c5fc8d0a74be4a2ab93bf1f";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+let now = new Date();
+let dayAndTimeElement = document.querySelector("#dayAndTime");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
+
+dayAndTimeElement.innerHTML = `${day} ${hour}:${minutes}`;
