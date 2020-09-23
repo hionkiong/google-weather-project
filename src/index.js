01ca92cf186dc9400e8f81ecaa9992410217bf05
hionkiong <stellachoi007@gmail.com>
2020-09-23 22:38:04 +0100

    Updated time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+      hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10 {
+      minutes = `0${minutes}`;
+  })
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -11,27 +34,13 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = response.data.wind.speed;
+  let dayAndTimeElement = document.querySelector("#dayAndTime");
+  dayAndTimeElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  console.log = response.data.dt;
 }
 
 let apiKey = "d20ee31d1c5fc8d0a74be4a2ab93bf1f";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
-
-let now = new Date();
-let dayAndTimeElement = document.querySelector("#dayAndTime");
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hour = now.getHours();
-let minutes = now.getMinutes();
-
-dayAndTimeElement.innerHTML = `${day} ${hour}:${minutes}`;
